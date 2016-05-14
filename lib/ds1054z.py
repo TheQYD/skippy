9d74bc62cf830457299adbee239e53abba5994dc
TheQYD <emailqasim@gmail.com>
2016-05-14 00:20:42 -0400

    5/13/16

--- lib/ds1054z.py
+++ lib/ds1054z.py
@@ -1,8 +1,30 @@
 class Math(object):
-  
-  def Operator(self, arg, flag, value=None):
-    scpi_command = arg + ':' + flag + value[0]
-    return scpi.upper()
-    
-    
+
+  @classmethod
+  def Operator(cls, module, pos=None, flag=None, value=None):
+    scpi_command = module + ':' + flag + value[0]
+    return scpi_command.upper()
+
+
+class Channel(object):
+
+  @classmethod
+  def Scale(cls, module, pos=None, flag=None, value=None):
+    if pos is not None:
+      module = module + pos
+    scpi_command = module + ':' + flag + ':' + value[0]
+    return scpi_command.upper()
+
+
+class Channel1(Channel):
+  pass
+
+class Channel2(Channel):
+  pass
+
+class Channel3(Channel):
+  pass
+
+class Channel4(Channel):
+  pass
 
--- skippy.py
+++ skippy.py
@@ -24,33 +24,39 @@ def DisconnectFromDevice(connected_device):
 def ProcessArgs(config):
   parser = argparse.ArgumentParser( version='lxi-cmd.py 1.0')
   subparsers = parser.add_subparsers(help='commands')
-  
+
   # Looping through arguments.
   for arg in config:
     arg_data = config[arg]
     arg_help = arg_data['help']
     positional_argument = subparsers.add_parser(arg, help=arg_help)
-    
+
     # Processing args.
     flags = arg_data['flags']
     if flags is not None:
       for flag in flags:
-        flag_help = flags[flag]['help'] 
+        flag_help = flags[flag]['help']
         flag = '--' + flag
         positional_argument.add_argument(flag, action='store', nargs='*', help=flag_help)
         positional_argument.set_defaults(__subparser__=arg)
   return parser.parse_args()
 
-if __name__ == '__main__':
-  config = yaml.load(open('config/rigol-ds1054z.yaml'))
+def CallCommand(config):
+  config = yaml.load(open(config))
   args = ProcessArgs(config)
   module = args.__subparser__.capitalize()
   args_dict = vars(args)
-  args_dict.pop('__subparser__') 
+  args_dict.pop('__subparser__')
+
   for flag in args_dict:
     if args_dict[flag] is not None:
        value = args_dict[flag]
        flag = flag.capitalize()
        module_call = getattr(ds1054z, module)
        method_call = getattr(module_call, flag)
-       method_call(module, flag, value)
+       scpi_command = method_call(module, flag, value)
+
+  print scpi_command
+
+if __name__ == '__main__':
+  CallCommand('config/rigol-ds1054z.yaml')
